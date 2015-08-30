331bd9bed8848ea07ad84f445a0063bcf91550a1
MizukiNakano <mizuki590@gmail.com>
2015-08-30 14:40:48 +0900

    Initial commit

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class UnityChanController : MonoBehaviour {
+	
+	private Animator animator;
+	private int doWalkId;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		doWalkId = Animator.StringToHash ("is_running");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			animator.SetBool(doWalkId, true);
+		}else{
+			animator.SetBool(doWalkId, false);
+		}
+	}
+}
