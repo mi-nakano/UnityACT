2d20f48cc8427fc033aefc460457e023ba097089
MizukiNakano <mizuki590@gmail.com>
2015-08-30 15:15:32 +0900

    Enable to move

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -3,19 +3,26 @@ using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 public class UnityChanController : MonoBehaviour {
-	
+
+	private CharacterController controller;
 	private Animator animator;
 	private int runId;
+
+	private Vector3 moveVector;
 	
 	// Use this for initialization
 	void Start () {
+		controller = GetComponent<CharacterController> ();
 		animator = GetComponent<Animator> ();
 		runId = Animator.StringToHash ("is_running");
+		moveVector = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.UpArrow)) {
+			moveVector = new Vector3(0, 0, 0.1F);
+			controller.Move(moveVector);
 			animator.SetBool(runId, true);
 		}else{
 			animator.SetBool(runId, false);
