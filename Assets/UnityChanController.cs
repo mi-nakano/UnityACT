7677268792c18f1b78f8a27a0faaa5dc6ae7ba65
MizukiNakano <mizuki590@gmail.com>
2015-08-30 15:33:51 +0900

    enable to move to 4directions

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -3,29 +3,47 @@ using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 public class UnityChanController : MonoBehaviour {
+	private const float SPEED = 0.1F; 
 
 	private CharacterController controller;
 	private Animator animator;
 	private int runId;
-
 	private Vector3 moveVector;
-	
+
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController> ();
 		animator = GetComponent<Animator> ();
 		runId = Animator.StringToHash ("is_running");
 		moveVector = Vector3.zero;
-	}
-	
+		}
+		
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			moveVector = new Vector3(0, 0, 0.1F);
-			controller.Move(moveVector);
-			animator.SetBool(runId, true);
-		}else{
-			animator.SetBool(runId, false);
+		setMoveVector();
+		if (moveVector.Equals (Vector3.zero)) {		// stop
+			animator.SetBool (runId, false);
+		} else {									// running
+			animator.SetBool (runId, true);	
+			controller.Move (moveVector * SPEED);
+			transform.rotation = Quaternion.LookRotation(moveVector);
 		}
+	}
+
+	private void setMoveVector(){
+		moveVector = Vector3.zero;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			moveVector.z += 1;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			moveVector.z -= 1;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			moveVector.x += 1;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			moveVector.x -= 1;
+		}
+		moveVector.Normalize();
 	}
 }
