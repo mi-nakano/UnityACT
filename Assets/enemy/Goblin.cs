5475b301ba0827e202d2555f84b54a4a3b4c746c
MizukiNakano <mizuki590@gmail.com>
2015-09-01 21:44:29 +0900

    goblin move forward to player

--- Assets/enemy/Goblin.cs
+++ Assets/enemy/Goblin.cs
@@ -2,23 +2,39 @@
 using System.Collections;
 
 public class Goblin : MonoBehaviour {
-	private const int SPEED = 2;
+	private const float SPEED = 0.05F;
 	private const float SEARCH_DISTANCE = 8F;
+	private const float ATTACK_DISTANCE = 1F;
 
 	private Transform player;
+	private CharacterController controller;
 	private bool consious;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		controller = GetComponent<CharacterController> ();
 		consious = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (consious == false && Vector3.Distance(player.position, gameObject.transform.position) < SEARCH_DISTANCE) {
+		Vector3 heading = player.position - transform.position;
+		if (consious == false && heading.magnitude < SEARCH_DISTANCE) {
 			print ("Goblin become consiousness!");
 			consious = true;
+		}
+		if (consious == true) {
+			think(heading);
+		}
+	}
+
+	private void think (Vector3 heading){
+		transform.rotation = Quaternion.LookRotation (heading);
+		if (heading.magnitude < ATTACK_DISTANCE) {
+			print ("Goblin attack!");
+		} else {
+			controller.Move(heading.normalized * SPEED);
 		}
 	}
 }
