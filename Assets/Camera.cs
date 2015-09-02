e6ec5773b8e2d40ddf271074cecae70c47f7eefb
MizukiNakano <mizuki590@gmail.com>
2015-09-02 22:47:14 +0900

    modified camer position

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -3,19 +3,20 @@ using System.Collections;
 
 public class Camera : MonoBehaviour {
 
-	private const float SMOOTHING = 5F;
+	private const float SMOOTHING = 100F;
+	private Vector3 offset = new Vector3 (1.8F, -1F, 1.8F);
 	private GameObject player;
-	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
-		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetCamPos = player.transform.position + offset;
+		Vector3 targetCamPos = player.transform.position - new Vector3(transform.forward.x * offset.x,
+		                                                               offset.y,
+		                                                               transform.forward.z * offset.z);
 		transform.position = Vector3.Lerp (transform.position, targetCamPos, SMOOTHING * Time.deltaTime);	
 	}
 }
