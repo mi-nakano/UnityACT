0c21d00947fb5125805eeb4bc34c1dd369eedf49
MizukiNakano <mizuki590@gmail.com>
2015-09-12 16:12:32 +0900

    add goblinHand's colider

---
+++ Assets/GoblinHand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoblinHand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+		print ("Colide goblinHand");
+	}
+}
