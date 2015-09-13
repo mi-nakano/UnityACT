51c127b05472c903b56ca7f7f2ca6ad27e3e4f7f
MizukiNakano <mizuki590@gmail.com>
2015-09-13 23:04:45 +0900

    add bruce controller

---
+++ Assets/enemy/Bruce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bruce : AbstractEnemy {
+	public int MAX_HP;
+
+	override protected void Init(){
+		hp = MAX_HP;
+	}
+
+	override protected void Alive () {
+	}
+
+	override protected void Death (){
+		throw new System.NotImplementedException ();
+	}
+}
