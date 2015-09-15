e61a5ec7a404f1e905871cc5345ec322284d511a
MizukiNakano <mizuki590@gmail.com>
2015-09-15 21:44:51 +0900

    add AbstractEnemyAttack

---
+++ Assets/enemy/AbstractEnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractEnemyAttack : MonoBehaviour {
+
+	protected bool isColide;
+	protected bool isHited;
+
+	// Use this for initialization
+	void Start () {
+		Init ();
+	}
+
+	virtual public void Init(){
+		isColide = false;
+		isHited = false;
+	}
+
+	protected bool isCollideToPlayer (Collision col){
+		if (col.gameObject.tag.Equals ("Player"))
+			return true;
+		return false;
+	}
+
+	public bool IsFirstHited(){
+		if (isColide == true && isHited == false) {
+			isHited = true;
+			return true;
+		}
+		return false;
+	}
+}
--- Assets/enemy/GoblinHand.cs
+++ Assets/enemy/GoblinHand.cs
@@ -1,37 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoblinHand : MonoBehaviour {
-	bool isColide;
-	bool isHited;
-
-	// Use this for initialization
-	void Start () {
-		Init ();
-	}
-
-	public void Init(){
-		isColide = false;
-		isHited = false;
-	}
+public class GoblinHand : AbstractEnemyAttack {
 
 	void OnCollisionEnter (Collision col) {
-		string tag = col.gameObject.tag;
-		if (!tag.Equals ("Player")) return;
-		isColide = true;
+		if (isCollideToPlayer (col)) {
+			isColide = true;
+		}
 	}
 
 	void OnCollisionExit(Collision col){
-		if (!tag.Equals ("Player")) return;
-		isColide = false;
-		isHited = false;
-	}
-
-	public bool IsFirstHited(){
-		if (isColide == true && isHited == false) {
-			isHited = true;
-			return true;
+		if (isCollideToPlayer (col)) {
+			Init();
 		}
-		return false;
 	}
 }
