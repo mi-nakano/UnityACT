a7a685e486da3daea77115ff8a7ed71ceff6157a
MizukiNakano <mizuki590@gmail.com>
2015-09-15 22:56:44 +0900

    add attack motion to bruce

--- Assets/enemy/Bruce.cs
+++ Assets/enemy/Bruce.cs
@@ -3,12 +3,38 @@ using System.Collections;
 
 public class Bruce : AbstractEnemy {
 	public int MAX_HP;
+	public AbstractEnemyAttack LSpear;
+	public AbstractEnemyAttack RSpear;
 
+	private int COUNT = 200;
+
+	private Animation animation;
+	private int counter;
+
+	
 	override protected void Init(){
+		base.Init ();
 		hp = MAX_HP;
+		animation = (Animation)GetComponent<Animation> ();
+		counter = 0;
 	}
 
 	override protected void Alive () {
+		if (counter == 0) {
+			Attack ();
+			counter ++;
+		} else if(animation.IsPlaying("spin")){
+		} else {
+			animation.Play("wait");
+			counter ++;
+			if (counter > COUNT) counter = 0;
+		}
+	}
+
+	private void Attack(){
+		animation.Play ("spin");
+		LSpear.Activate ();
+		RSpear.Activate ();
 	}
 
 	override protected void Death (){
